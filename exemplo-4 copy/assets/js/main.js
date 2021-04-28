0dd301893d4fddd9afecfe493ed5ab058f6b652a
Lucas Leite <carvalho1104@hotmail.com>
2021-04-28 10:04:44 -0300

    calculo de IMC

--- exemplo-4 copy/assets/js/main.js
+++ exemplo-4 copy/assets/js/main.js
@@ -3,7 +3,6 @@ const form = document.querySelector('.formaulario');
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
-  setResultado('Olá Mundo!'); 
 
 
   const inputPeso = e.target.querySelector('#peso'); 
@@ -13,8 +12,52 @@ form.addEventListener('submit', function(e) {
   const altura = Number(inputAltura.value);
   
   console.log(peso, altura)
+ 
+  // se o peso for falso ele encerra o programa aqui.
+  if(!peso){
+    setResultado('Peso inválido', false); 
+    return 
+  }
 
-}); 
+  if (!altura){
+    setResultado('altura inválida', false); 
+    return; 
+  }
+
+  const imc = getImc(peso, altura); 
+  const nivelImc = getNivelImc(imc);
+  
+  const msg = `Seu IMC é ${imc} (${nivelImc})`; 
+
+  setResultado (msg, true);1 
+
+});
+
+function getNivelImc(imc){
+  const nivel = [ 'Abaixo do peso', 'Peso Normal', 'Sobrepeso', 
+'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+
+
+if(imc >= 39.9) return nivel[5]
+
+if (imc >= 34.90) return nivel[4]
+
+if (imc >= 29.9) return nivel[3]
+
+if (imc >= 24.90) return nivel[2]; 
+
+if (imc >= 18.5 ) return nivel[1]
+
+if(imc < 18.5) return nivel[0]; 
+
+
+}
+
+function getImc(peso, altura){
+  const imc = peso / altura ** 2;  
+  return imc.toFixed(2); 
+
+}
 
 
 function criaP(className){
@@ -22,12 +65,19 @@ function criaP(className){
     return p;     
 }
 
-function setResultado(msg){
+function setResultado(msg, isValid){
     const resultado = document.querySelector('.resultado'); 
     resultado.innerHTML = ''; 
+    
     const p = criaP(); 
+    if(isValid){
+      p.classList.add('paragrafo-resultado'); 
+    }else {
+      p.classList.add('invalido'); 
+    }
 
-    resultado.appendChild(p); 
-    p.innerHTML = 'Qualquer coisa'; 
+  
+    p.innerHTML = msg; 
+    resultado.appendChild(p);
 }
 
