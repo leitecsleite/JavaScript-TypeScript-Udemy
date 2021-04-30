906186f7249bfc57dfd6278be1d00526be5c7433
Lucas Leite <carvalho1104@hotmail.com>
2021-04-30 10:57:53 -0300

    For -clássico - Estrutura de repetição

---
+++ 15-EX.js
@@ -0,0 +1,20 @@
+
+
+
+// i-index; 
+for (let i = 0; i <= 10; i++){
+    const par = i % 2 === 0 ? 'par' : 'ímpar'; 
+    console.log(i, par); 
+}
+
+//imprimir : 0 par
+//1 ímpar
+//2 par
+//3 ímpar
+//4 par
+//5 ímpar
+//6 par
+//7 ímpar
+//8 par
+//9 ímpar
+//10 par
---
+++ 16-EX.js
@@ -0,0 +1,15 @@
+
+
+const frutas = ['Maça', 'Pêra', 'Uva']; 
+
+for (let i = 0; i < frutas.length; i++){
+    console.log('fruta: ' + frutas[i]); 
+}
+
+//imprimir: 
+/**
+ * fruta: Maça
+   fruta: Pêra
+   fruta: Uva
+ * 
+ */
