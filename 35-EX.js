a18f48ffb4bfe86801e8ab5ae5b19192d8e7423c
Lucas Leite <carvalho1104@hotmail.com>
2021-05-10 11:15:06 -0300

    array- revisão

---
+++ 35-EX.js
@@ -0,0 +1,14 @@
+//
+
+const nomes = ['Eduardo', 'Maria', 'Joana']; 
+const novo = [...nomes]
+
+novo.pop();
+console.log(nomes); 
+console.log(novo); 
+
+
+//[ 'Eduardo', 'Maria', 'Joana' ]
+//[ 'Eduardo', 'Maria' ]
+
+//String, objetos, funções , números; 
