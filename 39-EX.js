13e8e3a3c8c4a4ba97978a1d087d1388e7f0326a
Lucas Leite <carvalho1104@hotmail.com>
2021-05-10 12:00:21 -0300

    Array-slice

--- 39-EX.js
+++ 39-EX.js
@@ -1,5 +1,7 @@
-
+// slice - > 
 
 const nomes = ['Eduardo', 'Maria', 'Joana', 'Arthur', 'Lucas'];
 const novo = nomes.slice(0, -1); 
 console.log(novo); 
+
+//impirmir: [ 'Eduardo', 'Maria', 'Joana', 'Arthur' ]
