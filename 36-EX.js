121ce85860c1d1a56734c519221c9b83b23cab05
Lucas Leite <carvalho1104@hotmail.com>
2021-05-10 11:39:39 -0300

    Array-unshift

---
+++ 36-EX.js
@@ -0,0 +1,8 @@
+// adiciona o nome no primeiro indice; 
+const nomes = ['Eduardo','Maria', 'Joana']; 
+nomes.unshift('João'); 
+nomes.unshift('Wallace'); 
+
+console.log(nomes); 
+
+//Imprimir: [ 'Wallace', 'João', 'Eduardo', 'Maria', 'Joana' ]
