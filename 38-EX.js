4f83eafc1bdca54c42e82de4c8c12bd44019f03d
Lucas Leite <carvalho1104@hotmail.com>
2021-05-10 11:49:28 -0300

    array-pop()

---
+++ 38-EX.js
@@ -0,0 +1,11 @@
+// pop () -> elimina o último elemento; 
+
+const nomes = ['Eduardo', 'Maria', 'Joana'];
+const novo = [...nomes]; 
+
+novo.pop(); 
+console.log(nomes); 
+console.log(novo); 
+
+//Imprimmir: [ 'Eduardo', 'Maria', 'Joana' ]
+//[ 'Eduardo', 'Maria' ]
