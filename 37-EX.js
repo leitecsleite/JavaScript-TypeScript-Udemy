d7448ebf3e3da345b56306f118139bef4fcb005a
Lucas Leite <carvalho1104@hotmail.com>
2021-05-10 11:42:30 -0300

    array-push

---
+++ 37-EX.js
@@ -0,0 +1,7 @@
+//adiciona o nome no último indíce; 
+
+const nomes = ['Eduardo', 'Maria', 'Joana']; 
+nomes.push('João'); 
+console.log(nomes); 
+
+//impirmir: [ 'Eduardo', 'Maria', 'Joana', 'João' ] ; 
