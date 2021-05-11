73ea4f5082f5d664fdc181b3aaecead8f1b98a35
Lucas Leite <carvalho1104@hotmail.com>
2021-05-11 11:17:26 -0300

    array-splice()

---
+++ 41-EX.js
@@ -0,0 +1,8 @@
+
+//               -5         -6       -7       -8        -9 
+//                1          2        3        4         5  
+const nomes = ['Eduardo', 'Maria', 'Joana', 'Gabriel','Júlio']; 
+
+const removidos = nomes.splice(3, 2, 'Luiz'); 
+console.log(nomes, removidos); 
+
