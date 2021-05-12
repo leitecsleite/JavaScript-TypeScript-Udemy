fd7ec00fb4b5f224c8e2ac58d53e074692261f62
Lucas Leite <carvalho1104@hotmail.com>
2021-05-12 11:21:39 -0300

    map(), filter(), reduce()

--- 52-EX.js
+++ 52-EX.js
@@ -4,14 +4,13 @@
 
 const numeros = [5,50,80,1, 2, 3, 5, 8, 7, 11, 15, 22, 27]; 
 
-const numerosPares = numeros.filter(valor => {
-    return valor % 2 === 0; 
-}).map(valor =>{
-    return valor * 2; 
-}).reduce((acumulador, valor) => {
-    return acumulador += valor; 
-}, 0); 
+const numerosPares = numeros
+    .filter(valor =>  valor % 2 === 0)
+    .map(valor => valor * 2)
+    .reduce((acumulador, valor) =>  acumulador += valor); 
+
+
 // [50, 80, 2, 8, 22] pares
-// [100, 160, 4, 16, 44]
-// 324;
+// [100, 160, 4, 16, 44] Dobro
+// 324; soma total de todos os pares; 
 console.log(numerosPares); 
