6162e0fce82bdba0d3c75f24cf0243320d506e57
Lucas Leite <carvalho1104@hotmail.com>
2021-05-20 12:15:39 -0300

    Class

---
+++ JavaScript-Classes/01-EX.js
@@ -0,0 +1,16 @@
+
+//Classes; 
+
+class Pessoa{
+    constructor(nome, sobrenome){
+        this.nome = nome; 
+        this.sobrenome = sobrenome; 
+    }
+
+    falar(){
+        console.log(`${this.nome} está falando.`); 
+    }
+}
+
+const p1 = new Pessoa('Lucas', 'Leite'); 
+p1.falar();
