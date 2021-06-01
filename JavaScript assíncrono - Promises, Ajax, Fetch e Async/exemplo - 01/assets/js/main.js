c87da39a2b4c2ed52d3b31da198614013d0556bf
Lucas Leite <carvalho1104@hotmail.com>
2021-06-01 13:36:46 -0300

    Update main.js

--- JavaScript assíncrono - Promises, Ajax, Fetch e Async/exemplo - 01/assets/js/main.js
+++ JavaScript assíncrono - Promises, Ajax, Fetch e Async/exemplo - 01/assets/js/main.js
@@ -1,51 +1,60 @@
 
 
 const request = obj => {
-    const xhr = new XMLHttpRequest(); 
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
 
-    xhr.open(obj.method, obj.url, true); 
-    xhr.send(); 
+        xhr.open(obj.method, obj.url, true);
+        xhr.send();
 
-    xhr.addEventListener('load', () => {
-        if(xhr.status >= 200 && xhr.status < 300){
-            obj.success(xhr.responseText); 
-        } else { 
-            obj.error ({
-                code: xhr.status,
-                msg: xhr.statusText
-            }); 
-        }
-    }); 
-}; 
+        xhr.addEventListener('load', () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr.responseText);
+            } else {
+                reject({
+                    code: xhr.status,
+                    msg: xhr.statusText
+                });
+            }
+        });
+    });
+};
 
 
 document.addEventListener('click', e => {
     const el = e.target;
-    const tag = el.tagName.toLowerCase(); 
+    const tag = el.tagName.toLowerCase();
 
-    if (tag === 'a'){
-        e.preventDefault(); 
-        carregaPagina(el); 
+    if (tag === 'a') {
+        e.preventDefault();
+        carregaPagina(el);
     }
-}); 
+});
 
-function carregaPagina (el){
-    const href = el.getAttribute('href'); 
+ async function carregaPagina(el) {
+    const href = el.getAttribute('href');
     console.log(href);
-    
-    request({
-        method:'GET', 
+
+    const objConfig = {
+        method: 'GET', 
         url: href, 
-        success(response){
-            carregaResultado(response); 
-        }, 
-        error(errorText){
-            console.log(errorText); 
-        }
-    }); 
+    };  
+
+    try {
+    const  response = await request(objConfig);
+    carregaResultado(response);
+    } catch (e){
+        console.log(e); 
+    }
+
+    //request(objConfig).then(response => {
+    //    carregaResultado(response); 
+    //}).catch(error => {
+    //  console.log(error); 
+   // })
 }
 
- function carregaResultado(response){
+function carregaResultado(response) {
     const resultado = document.querySelector('.resultado');
-    resultado.innerHTML = response; 
+    resultado.innerHTML = response;
 }
