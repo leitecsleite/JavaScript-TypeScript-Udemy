f3887d8a630314eb1864493cf00aa2b5f67ff1a9
Lucas Leite <carvalho1104@hotmail.com>
2021-06-07 14:19:53 -0300

    import/export

--- JavaScript- Tooling e ES6 Modules - Módulo/exemplo-03/src/modulo1.js
+++ JavaScript- Tooling e ES6 Modules - Módulo/exemplo-03/src/modulo1.js
@@ -8,7 +8,7 @@ export default function soma(x, y){
     return x + y; 
 }
 
-export class Pessoa {
+export  default class Pessoa {
     constructor(nome, sobrenome){
         this.nome = nome; 
         this.sobrenome = sobrenome;
