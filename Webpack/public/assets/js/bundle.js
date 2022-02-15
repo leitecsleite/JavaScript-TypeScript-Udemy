dffa7470370f9349481bb1d463e1efd4ec875fa7
Lucas <carvalho1104@hotmail.com>
2022-02-15 12:03:00 -0300

    webpack - 15/02/2022

---
+++ Webpack/public/assets/js/bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+alert('Olá Mundo');
+console.log('funcinou');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ Webpack/src/index.js
@@ -0,0 +1,3 @@
+alert('Olá Mundo')
+
+console.log('funcinou')
